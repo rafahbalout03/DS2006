c435991039175cdff44968bd89d39b5921e69851
Elham Zamar <u4318035396@gmail.com>
2025-11-08 13:51:48 +0100

    Added user input for dice sides in dice_simulation.py

--- Lab1_Dice_Problem/Lab1_Dice_Problem/dice_simulation.py
+++ Lab1_Dice_Problem/Lab1_Dice_Problem/dice_simulation.py
@@ -4,6 +4,7 @@ def roll_dice ():
     return random.randint(1, 6)
 
 if __name__ == "__main__":
+    sides = int(input("How many sides should the dice have? "))
     print("Simulating a dice roll...")
     result = roll_dice()
     print(f"You rolled a {result}")
